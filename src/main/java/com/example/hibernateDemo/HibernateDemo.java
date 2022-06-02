df4586e0190d0aa537aa39b651c48cabc17f0b89
sonny <sonnyshih@gmail.com>
2022-06-02 11:17:36 +0800

    1. Develop the Hibernate Demo.

--- src/main/java/com/example/hibernateDemo/HibernateDemo.java
+++ src/main/java/com/example/hibernateDemo/HibernateDemo.java
@@ -1,30 +1,33 @@
 package com.example.hibernateDemo;
 
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateDemo {
     public static void main(String[] args) {
 
-        Alien alien = new Alien();
-        alien.setAid(101);
-        alien.setAname("Hello");
-        alien.setColor("Green");
+        EmployeeEntity emp = new EmployeeEntity();
+        emp.setEmail("demo-user@mail.com");
+        emp.setFirstName("demo");
+        emp.setLastName("user");
 
-        try {
-            Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
-            SessionFactory sf = con.buildSessionFactory();
-            Session session = sf.openSession();
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metaData = new MetadataSources(standardRegistry)
+                                .getMetadataBuilder()
+                                .build();
 
-            Transaction tx = session.beginTransaction();
-            session.save(alien);
+        SessionFactory sessionFactory = metaData.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(emp);
+        session.getTransaction().commit();
+        sessionFactory.close();
 
 
-        } catch (HibernateException e) {
-
-        }
     }
 }
