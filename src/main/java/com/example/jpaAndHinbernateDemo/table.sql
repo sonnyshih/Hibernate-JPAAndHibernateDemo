3772d0dc986bcfdd94dc9230b24f639afeb2e536
sonny <sonnyshih@gmail.com>
2022-06-02 14:46:42 +0800

    1. Add the creating table.sql

---
+++ src/main/java/com/example/jpaAndHinbernateDemo/table.sql
@@ -0,0 +1,23 @@
+-- 員工
+CREATE TABLE Employee
+(
+    ID        bigint NOT NULL AUTO_INCREMENT,
+    FirstName      VARCHAR(50) COMMENT '姓名',
+    LastName   VARCHAR(50) COMMENT '帳號',
+    DeptID bigint not null default '0',
+    PRIMARY KEY (ID)
+) COMMENT '員工'
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 DEFAULT COLLATE=utf8mb3_general_ci;
+
+-- 部門
+CREATE TABLE Department
+(
+    ID        bigint NOT NULL AUTO_INCREMENT,
+    Name      VARCHAR(50) COMMENT '部門名稱',
+    PRIMARY KEY (ID)
+) COMMENT '部門'
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 DEFAULT COLLATE=utf8mb3_general_ci;
+
+ALTER TABLE
+    `hibernate_demo`.`Employee` ADD CONSTRAINT Employee_fk1 FOREIGN KEY (`DeptID`) REFERENCES
+    `hibernate_demo`.`Department` (`ID`)
